4468fff2947b139cc4e672086aa58df67618af39
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-01-29 22:50:01 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+import { stdout } from "process";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  let __filename = fileURLToPath(import.meta.url);
+  let __dirname = path.dirname(__filename);
+  const hash = createHash("sha256");
+  const input = createReadStream(
+    path.join(__dirname, "files/fileToCalculateHashFor.txt")
+  );
+
+  input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
