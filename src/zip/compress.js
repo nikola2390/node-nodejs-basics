81c8f131e1fb197693f17d8d82b50dc6ea707ce4
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-01-29 23:00:04 +0300

    feat: implement zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+import { getDirName, getFileName } from "../utils/utils.js";
+import { join } from "path";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  let __filename = getFileName(import.meta.url);
+  let __dirname = getDirName(__filename);
+
+  await pipeline(
+    createReadStream(join(__dirname, "./files/fileToCompress.txt")),
+    gzip,
+    createWriteStream(join(__dirname, "./files/archive.gz"))
+  );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { pipeline } from "stream/promises";
+import { createReadStream, createWriteStream } from "fs";
+import { getDirName, getFileName } from "../utils/utils.js";
+import { join } from "path";
+import { createUnzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createUnzip();
+  let __filename = getFileName(import.meta.url);
+  let __dirname = getDirName(__filename);
+
+  await pipeline(
+    createReadStream(join(__dirname, "./files/archive.gz")),
+    unzip,
+    createWriteStream(join(__dirname, "./files/fileToCompress.txt"))
+  );
 };
 
 await decompress();
