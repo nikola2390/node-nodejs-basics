25b67cc0b676ffd8d26ac76a95845cf5265b98aa
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-01-29 23:04:58 +0300

    faet: implement child processes tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from "child_process";
+import { getDirName, getFileName } from "../utils/utils.js";
+import path from "path";
+
+const __filename = getFileName(import.meta.url);
+const __dirname = getDirName(__filename);
+const childUrl = path.join(__dirname, "./files/script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(childUrl, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([]);
