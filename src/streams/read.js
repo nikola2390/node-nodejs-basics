d75053d4574ebc14de5b365fed46942b00e0f625
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-01-29 22:55:09 +0300

    feat: implement streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { getDirName, getFileName } from "../utils/utils.js";
+import path from "path";
+import { createReadStream } from "fs";
+import { pipeline } from "stream/promises";
+
 const read = async () => {
-    // Write your code here 
+  let __filename = getFileName(import.meta.url);
+  let __dirname = getDirName(__filename);
+
+  await pipeline(
+    createReadStream(path.join(__dirname, "files/fileToRead.txt")),
+    process.stdout
+  );
 };
 
 await read();
