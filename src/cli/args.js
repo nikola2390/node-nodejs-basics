7a15be44dceb783269e7b46140a12e0f9b2f4149
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-01-29 22:45:44 +0300

    feat: implement command line tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,8 @@
 const parseArgs = () => {
-    // Write your code here 
+  let values = process.argv.slice(2);
+  for (let i = 0; i < values.length; i += 2) {
+    console.log(`${values[i].slice(2)} is ${values[i + 1]}`);
+  }
 };
 
 parseArgs();
