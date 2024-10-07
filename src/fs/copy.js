a08a44132b1a90d27f20c49f60e8772a33ee0c3e
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-10-07 18:41:26 +0300

    fix: fs-copy add check for the existence of the destination folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,6 +1,11 @@
 import { cp } from "node:fs/promises";
+import { existsSync } from "node:fs";
 
 const copy = async () => {
+  if (existsSync("./src/fs/files_copy")) {
+    throw Error("FS operation failed");
+  }
+
   cp("./src/fs/files", "./src/fs/files_copy", {
     recursive: true,
     errorOnExist: true,
